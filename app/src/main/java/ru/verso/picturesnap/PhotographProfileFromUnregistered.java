580dbb1c158ebeeae3d45fa8ddaed525dd32ca2b
Ruslan <ruslan.itpro@gmail.com>
2023-04-04 00:42:55 +0300

    Added photographer profile markings, which will be visible from the client side. Layout resources have also been refactored.

--- app/src/main/java/ru/verso/picturesnap/PhotographProfileFromUnregistered.java
+++ app/src/main/java/ru/verso/picturesnap/PhotographProfileFromUnregistered.java
@@ -8,12 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ClientPhotographProfile extends Fragment {
-    
+public class PhotographProfileFromUnregistered extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_client_photograph_profile, container, false);
+        return inflater.inflate(R.layout.fragment_photograph_profile_from_unregistered, container, false);
     }
 }
