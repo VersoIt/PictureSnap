0fa9bd10b88a7a07ca9ac042915b46389ff3a7b9
Ruslan <ruslan.itpro@gmail.com>
2023-04-04 13:53:06 +0300

    Added service actions fragments.

---
+++ app/src/main/java/ru/verso/picturesnap/presentation/fragments/client/ServiceSelectionForClient.java
@@ -0,0 +1,21 @@
+package ru.verso.picturesnap.presentation.fragments.client;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.verso.picturesnap.R;
+
+public class ServiceSelectionForClient extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_service_selection_for_client, container, false);
+    }
+}
