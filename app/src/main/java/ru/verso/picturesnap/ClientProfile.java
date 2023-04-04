d7c4153a4e771222eeb1a20b45834de3cfa2a8f2
Ruslan <ruslan.itpro@gmail.com>
2023-04-04 12:37:57 +0300

    Added client profile

--- app/src/main/java/ru/verso/picturesnap/ClientProfile.java
+++ app/src/main/java/ru/verso/picturesnap/ClientProfile.java
@@ -1,4 +1,4 @@
-package ru.verso.picturesnap.presentation.fragments.client;
+package ru.verso.picturesnap;
 
 import android.os.Bundle;
 
@@ -8,14 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import ru.verso.picturesnap.R;
-
-public class Favorites extends Fragment {
+public class ClientProfile extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        return inflater.inflate(R.layout.fragment_client_profile, container, false);
     }
 }
