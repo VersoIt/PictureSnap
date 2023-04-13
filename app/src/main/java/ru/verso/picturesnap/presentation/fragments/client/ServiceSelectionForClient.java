ff2f7cb7c30563c78ef367a73fcf5be5504d44df
Ruslan <ruslan.itpro@gmail.com>
2023-04-13 22:38:45 +0300

    Added favorites fragment.

--- app/src/main/java/ru/verso/picturesnap/presentation/fragments/client/ServiceSelectionForClient.java
+++ app/src/main/java/ru/verso/picturesnap/presentation/fragments/client/ServiceSelectionForClient.java
@@ -18,4 +18,14 @@ public class ServiceSelectionForClient extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_service_selection_for_client, container, false);
     }
+
+    public static class Favorites extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_favorites, container, false);
+        }
+    }
 }
