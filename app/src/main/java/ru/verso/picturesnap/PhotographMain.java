2dcbc849181d9d35559a104540681c3b9dc65886
Ruslan <ruslan.itpro@gmail.com>
2023-04-13 22:19:26 +0300

    Added photograph main fragment.

--- app/src/main/java/ru/verso/picturesnap/PhotographMain.java
+++ app/src/main/java/ru/verso/picturesnap/PhotographMain.java
@@ -8,12 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Notifications extends Fragment {
+public class PhotographMain extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        return inflater.inflate(R.layout.fragment_photograph_main, container, false);
     }
 }
